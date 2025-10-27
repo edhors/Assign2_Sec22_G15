0896d7af552cbf465dc952346a8d454e1f3f7017
eduardo <horstmanneduardo100@gmail.com>
2025-10-28 01:17:11 +0400

    add: contructors, setters and getters, addEdge method, removeEdge method, removeVertex method and printGraph method

--- Collaboration_Sec22_G15.java
+++ Collaboration_Sec22_G15.java
@@ -1,11 +1,35 @@
 public class Collaboration_Sec22_G15 {
-    private Contributor_Sec22_G15 collaborator1;
-    private Contributor_Sec22_G15 collaborator2;
+    private Contributor_Sec22_G15 contributor1;
+    private Contributor_Sec22_G15 contributor2;
     private String projectId;
 
-    public Collaboration_Sec22_G15(Contributor_Sec22_G15 collaborator1, Contributor_Sec22_G15 collaborator2, String projectId) {
-        this.collaborator1 = collaborator1;
-        this.collaborator2 = collaborator2;
+    public Collaboration_Sec22_G15(Contributor_Sec22_G15 contributor1, Contributor_Sec22_G15 contributor2, String projectId) {
+        this.contributor1 = contributor1;
+        this.contributor2 = contributor2;
+        this.projectId = projectId;
+    }
+
+    public Contributor_Sec22_G15 getContributor1() {
+        return this.contributor1;
+    }
+
+    public Contributor_Sec22_G15 getContributor2() {
+        return this.contributor2;
+    }
+
+    public String getProjectId() {
+        return this.projectId;
+    }
+
+    public void setContributor1(Contributor_Sec22_G15 contributor1) {
+        this.contributor1 = contributor1;
+    }
+
+    public void setContributor2(Contributor_Sec22_G15 contributor2) {
+        this.contributor2 = contributor2;
+    }
+
+    public void setProjectId(String projectId) {
         this.projectId = projectId;
     }
 }
--- CommunityGraph_Sec22_G15.java
+++ CommunityGraph_Sec22_G15.java
@@ -1,18 +1,86 @@
 import java.util.LinkedList;
 
-public class CommunityGraph_Sec22_G15<E> {
+public class CommunityGraph_Sec22_G15 {
     private LinkedList<Contributor_Sec22_G15> vertices;
     private LinkedList<LinkedList<Collaboration_Sec22_G15>> adjacencyList;
 
-    /*
+    public CommunityGraph_Sec22_G15() {
+        this.vertices = new LinkedList<>();
+        this.adjacencyList = new LinkedList<>();
+    }
+
     public CommunityGraph_Sec22_G15(LinkedList<Contributor_Sec22_G15> vertices) {
         this.vertices = vertices;
-        this.adjList = new LinkedList<>();
-    } 
+        this.adjacencyList = new LinkedList<>();
+        while (adjacencyList.size() < vertices.size()) {
+            adjacencyList.add(new LinkedList<Collaboration_Sec22_G15>());
+        }
+    }
 
-    public CommunityGraph_Sec22_G15(LinkedList<LinkedList<Contributor_Sec22_G15>> neighbors) {
-        this.vertices = adjList.size();
-        this.adjList = adjList;
-    } 
-     */
+    public CommunityGraph_Sec22_G15(LinkedList<Contributor_Sec22_G15> vertices, LinkedList<LinkedList<Collaboration_Sec22_G15>> adjacencyList) {
+        this.vertices = vertices;
+        this.adjacencyList = adjacencyList;
+    }
+
+    public LinkedList<Contributor_Sec22_G15> getVertices() {
+        return this.vertices;
+    }
+
+    public LinkedList<LinkedList<Collaboration_Sec22_G15>> getAdjacencyList() {
+        return this.adjacencyList;
+    }
+
+
+    public void addEdge(Contributor_Sec22_G15 c1, Contributor_Sec22_G15 c2, String projectId) {
+        int index1 = vertices.indexOf(c1);
+        int index2 = vertices.indexOf(c2);
+    
+        if (index1 == -1 || index2 == -1) {
+            System.out.println("One or both contributors not found!");
+            return;
+        }
+    
+        //create collaboration objects (edges)
+        Collaboration_Sec22_G15 collaboration1 = new Collaboration_Sec22_G15(c1, c2, projectId);
+        Collaboration_Sec22_G15 collaboration2 = new Collaboration_Sec22_G15(c2, c1, projectId);
+    
+        //add the collaborations to both adjacency lists (undirected graph)
+        adjacencyList.get(index1).add(collaboration1);
+        adjacencyList.get(index2).add(collaboration2);
+    }
+    
+    public void removeEdge(Contributor_Sec22_G15 c1, Contributor_Sec22_G15 c2, String projectId) {
+        int index1 = vertices.indexOf(c1);
+        int index2 = vertices.indexOf(c2);
+    
+        if (index1 == -1 || index2 == -1) {
+            System.out.println("One or both contributors not found!");
+            return;
+        }
+    }
+
+    public void removeVertex(Contributor_Sec22_G15 contributor) {
+        int index = vertices.indexOf(contributor);
+    
+        if (index == -1) {
+            System.out.println("Contributor not found!");
+            return;
+        }
+    }
+
+    public void printGraph() {
+        for(Contributor_Sec22_G15 currentVertex : vertices) {
+            System.out.print(currentVertex.getName() + " -> ");
+            for(Collaboration_Sec22_G15 currentEdge : adjacencyList.get(vertices.indexOf(currentVertex))) {
+                System.out.print(currentEdge.getContributor2().getName());
+                if(currentEdge != adjacencyList.get(vertices.indexOf(currentVertex)).getLast()) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }    
+    }
+    
+    
+    
 }
