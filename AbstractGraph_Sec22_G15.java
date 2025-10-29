9c92abf79c3ff5cfe81ec71ab689e5e5cb1bfd95
eduardo <horstmanneduardo100@gmail.com>
2025-10-29 14:18:44 +0400

    create: Graph Interface and Abstract class AbstractGraph

---
+++ AbstractGraph_Sec22_G15.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+
+public abstract class AbstractGraph_Sec22_G15<V, E> implements Graph_Sec22_G15<V, E>{
+    protected LinkedList<V> vertices;
+    protected LinkedList<LinkedList<E>> adjacencyList;
+
+    public AbstractGraph_Sec22_G15() {
+        this.vertices = new LinkedList<>();
+        this.adjacencyList = new LinkedList<>();
+    }
+
+    public AbstractGraph_Sec22_G15(LinkedList<V> vertices) {
+        this.vertices = vertices;
+        this.adjacencyList = new LinkedList<>();
+        while (adjacencyList.size() < vertices.size()) {
+            adjacencyList.add(new LinkedList<E>());
+        }
+    }
+
+    public AbstractGraph_Sec22_G15(LinkedList<V> vertices, LinkedList<LinkedList<E>> adjacencyList) {
+        this.vertices = vertices;
+        this.adjacencyList = adjacencyList;
+    }
+
+    public LinkedList<V> getVertices() {
+        return this.vertices;
+    }
+
+    public LinkedList<LinkedList<E>> getAdjacencyList() {
+        return this.adjacencyList;
+    }
+
+    public abstract void addVertex(V vertex);
+    public abstract V removeVertex(V vertex);
+    public abstract void addEdge(V v1, V v2, String label);
+    public abstract void removeEdge(V v1, V v2, String label);
+    public abstract int influence(V start);
+
+    public void printGraph() {
+        for (int i = 0; i < vertices.size(); i++) {
+            System.out.print(vertices.get(i).toString());
+            for (E edge : adjacencyList.get(i)) {
+                System.out.print(" -> " + edge.toString());
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Graph_Sec22_G15.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public interface Graph_Sec22_G15<V, E> {
+    LinkedList<V> getVertices();    
+    LinkedList<LinkedList<E>> getAdjacencyList();
+    void addVertex(V vertex);
+    V removeVertex(V vertex);
+    void addEdge(V v1, V v2, String label);
+    void removeEdge(V v1, V v2, String label);
+    int influence(V start);
+    void printGraph();
+}
