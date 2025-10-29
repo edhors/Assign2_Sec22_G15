8df3ec9d1b652c5984f06c37f02350bea426a36b
eduardo <horstmanneduardo100@gmail.com>
2025-10-29 14:24:12 +0400

    add: occuption attribute

--- Individual_Sec22_G15.java
+++ Individual_Sec22_G15.java
@@ -1,5 +1,15 @@
 public class Individual_Sec22_G15 extends Contributor_Sec22_G15 {
-    public Individual_Sec22_G15(String id, String name) {
+    private String occupation;
+    public Individual_Sec22_G15(String id, String name, String occupation) {
         super(id, name);
+        this.occupation = occupation;
+    }
+
+    public String getOccupation() {
+        return this.occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
     }
 }
