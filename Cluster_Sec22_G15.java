548470e8f78f7eec79bd9580580558a2e3a97894
eduardo <horstmanneduardo100@gmail.com>
2025-10-30 11:34:29 +0400

    add: theme attribute add: new constructor with contributors LinkedList input field

--- Cluster_Sec22_G15.java
+++ Cluster_Sec22_G15.java
@@ -1,12 +1,37 @@
 import java.util.LinkedList;
 public class Cluster_Sec22_G15 extends Project_Sec22_G15 {
-    private String projectId;
     private LinkedList<Contributor_Sec22_G15> contributors;
+    private String theme;
 
-    public Cluster_Sec22_G15(String projectId) {
+    public Cluster_Sec22_G15(String projectId, String theme) {
         super(projectId);
+        this.theme = theme;
         this.contributors = new LinkedList<>();
     }
 
-    
+    public Cluster_Sec22_G15(String projectId, String theme, LinkedList<Contributor_Sec22_G15> contributors) {
+        super(projectId);
+        this.theme = theme;
+        this.contributors = contributors;
+    }
+
+    public void addContributor(Contributor_Sec22_G15 contributor) {
+        this.contributors.add(contributor);
+    }
+
+    public void removeContributor(Contributor_Sec22_G15 contributor) {
+        this.contributors.remove(contributor);
+    }
+
+    public LinkedList<Contributor_Sec22_G15> getContributors() {
+        return this.contributors;
+    }
+
+    public String getTheme() {
+        return this.theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
 }
