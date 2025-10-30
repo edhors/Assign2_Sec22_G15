f2e5efaf7dd8d9d8ab01451386d4cfaeca6f61eb
eduardo <horstmanneduardo100@gmail.com>
2025-10-30 14:45:06 +0400

    remove: unnecessary import

--- Collaboration_Sec22_G15.java
+++ Collaboration_Sec22_G15.java
@@ -1,5 +1,3 @@
-import java.util.Objects;
-
 public class Collaboration_Sec22_G15 extends Project_Sec22_G15 {
     private Contributor_Sec22_G15 contributor1;
     private Contributor_Sec22_G15 contributor2;
