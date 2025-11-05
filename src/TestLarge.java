7f03077b130e177972c3763b8dec9653afdb0a04
alitanbooz <alitanbooz@gmail.com>
2025-11-05 19:11:50 +0400

    Update TestLarge.java

--- src/TestLarge.java
+++ src/TestLarge.java
@@ -1,28 +1,37 @@
 public class TestLarge {
     public static void main(String[] args) {
         CommunityGraph_Sec22_G15 graph = new CommunityGraph_Sec22_G15();
+
         graph.addContributor(new Individual_Sec22_G15("I1", "Yousef", "Teacher"));
         graph.addContributor(new Individual_Sec22_G15("I2", "Sara", "Engineer"));
         graph.addContributor(new Individual_Sec22_G15("I3", "Omar", "Doctor"));
         graph.addContributor(new Individual_Sec22_G15("I4", "Lina", "Designer"));
         graph.addContributor(new Individual_Sec22_G15("I5", "Khalid", "Developer"));
+        graph.addContributor(new Individual_Sec22_G15("I6", "Maya", "Researcher"));
+        graph.addContributor(new Individual_Sec22_G15("I7", "Nadia", "Artist"));
+        graph.addContributor(new Individual_Sec22_G15("I8", "Faris", "Scientist"));
+        graph.addContributor(new Individual_Sec22_G15("I9", "Rami", "Programmer"));
+        graph.addContributor(new Individual_Sec22_G15("I10", "Huda", "Manager"));
 
         graph.addEdge(new Collaboration_Sec22_G15(
                 new Individual_Sec22_G15("I1", "Yousef", "Teacher"),
                 new Individual_Sec22_G15("I2", "Sara", "Engineer"),
-                "Project1"));
+                "Project Alpha"));
 
         graph.addEdge(new Collaboration_Sec22_G15(
                 new Individual_Sec22_G15("I3", "Omar", "Doctor"),
                 new Individual_Sec22_G15("I4", "Lina", "Designer"),
-                "Project2"));
+                "Project Beta"));
 
         graph.addEdge(new Collaboration_Sec22_G15(
                 new Individual_Sec22_G15("I5", "Khalid", "Developer"),
                 new Individual_Sec22_G15("I1", "Yousef", "Teacher"),
-                "Project3"));
+                "Project Gamma"));
 
-        System.out.println(" Large Dataset Test ");
-        graph.printGraph();
+        if (graph.getContributors().size() == 10 && graph.getCollaborations().size() == 3) {
+            System.out.println("Large Dataset  pass");
+        } else {
+            System.out.println("Large Dataset  fail");
+        }
     }
 }
