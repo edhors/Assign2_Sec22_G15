8b0dc6f7cbf82fe6f1fd6f77061d86c3908ea17e
alitanbooz <alitanbooz@gmail.com>
2025-11-05 16:27:38 +0400

    Update TestInvalid.java

--- src/TestInvalid.java
+++ src/TestInvalid.java
@@ -1,1 +1,12 @@
-
+public class TestInvalid {
+    public static void main(String[] args) {
+        CommunityGraph_Sec22_G15 graph = new CommunityGraph_Sec22_G15();
+        Contributor_Sec22_G15 a = new Individual_Sec22_G15("I1", "Ali", "Writer");
+        Contributor_Sec22_G15 b = new Individual_Sec22_G15("I2", "Edwardo", "Artist");
+        graph.addContributor(a);
+        graph.addEdge(new Collaboration_Sec22_G15(a, b, "InvalidProject"));
+        System.out.println("Invalid Collaborations Test");
+        graph.displayContributors();
+        graph.displayCollaborations(); 
+    }
+}
